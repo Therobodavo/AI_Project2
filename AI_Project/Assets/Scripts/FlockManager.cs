42928b0dc4b7d213f787fcb66c8570a908ab1a3b
Ventus-Ire <wasabi.3122@gmail.com>
2019-10-10 23:15:40 -0400

    Added Flock Changes and button presses

--- AI_Project/Assets/Scripts/FlockManager.cs
+++ AI_Project/Assets/Scripts/FlockManager.cs
@@ -7,6 +7,7 @@ public class FlockManager : MonoBehaviour
     public GameObject birdPrefab;
     public int numBird;
     public GameObject[] allBirds;
+    public List<GameObject> birds = new List<GameObject>();
     public Vector3 limit = new Vector3(5, 3, 5);
 
     public Vector3 goalPosition;
@@ -21,9 +22,28 @@ public class FlockManager : MonoBehaviour
     [Range(0.0f, 5.0f)]
     public float rotation;
 
+    //Adding Variables 
+    public KeyCode increaseAgents;
+    public KeyCode changeGoal;
+
+
     // Start is called before the first frame update
     void Start()
     {
+        
+        for (int i = 0; i < numBird; i++)
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-limit.x, limit.x),
+                                                                Random.Range(-limit.y, limit.y),
+                                                                Random.Range(-limit.z, limit.z));
+            birds.Add((GameObject)Instantiate(birdPrefab, pos, Quaternion.identity));
+        }
+
+        foreach(GameObject bird in birds)
+        {
+            bird.GetComponent<Flock>().myManager = this;
+        }
+        /*
         allBirds = new GameObject[numBird];
 
         for (int i = 0; i < numBird; i++)
@@ -34,7 +54,7 @@ public class FlockManager : MonoBehaviour
             allBirds[i] = (GameObject)Instantiate(birdPrefab, pos, Quaternion.identity);
             allBirds[i].GetComponent<Flock>().myManager = this;
         }
-
+        */
         goalPosition = this.transform.position;
 
     }
@@ -42,6 +62,8 @@ public class FlockManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        increaseAllBirds();
+        changeGoalPos();
         //Randomizes the Goal
         //if(Random.Range(0,100) < 10)
         //{
@@ -49,5 +71,32 @@ public class FlockManager : MonoBehaviour
               //                                                    Random.Range(-limit.y, limit.y),
               //                                                    Random.Range(-limit.z, limit.z));
         //}
+
+
+
+
+    }
+
+    void increaseAllBirds()
+    {
+        if (Input.GetKeyDown(increaseAgents))
+        {
+            Vector3 pos = this.transform.position + new Vector3(Random.Range(-limit.x, limit.x),
+                                                                Random.Range(-limit.y, limit.y),
+                                                                Random.Range(-limit.z, limit.z));
+            GameObject newBird = (GameObject)Instantiate(birdPrefab, pos, Quaternion.identity);
+            newBird.GetComponent<Flock>().myManager = this;
+
+            birds.Add(newBird);
+        }
+    }
+
+    void changeGoalPos()
+    {
+        if (Input.GetKeyDown(changeGoal))
+        {
+            Vector3 newPos = new Vector3(Random.Range(2, 35), this.transform.position.y, this.transform.position.z);
+            this.transform.position = newPos;
+        }
     }
 }
